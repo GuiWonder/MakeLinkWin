6cdbec02dbd278a233121dbc3d2b31589ce11509
agui <55447192+GuiWonder@users.noreply.github.com>
2022-05-10 21:02:56 -0500

    Update FormMain.Designer.cs

--- MakeLink/MakeLink/FormMain.Designer.cs
+++ MakeLink/MakeLink/FormMain.Designer.cs
@@ -199,7 +199,6 @@ namespace MakeLink
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MakeLink";
-            this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
